d24c61875d41370ea9f7b9e43372b9433b92621d
tonyxuan2021 <tonyyexuan@gmail.com>
2022-07-18 12:29:12 -0700

    save version before implementing asyncthunk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./components/CartContainer";
+import Modal from "./components/Modal";
 import Navbar from "./components/Navbar";
 import { calculateTotals } from "./features/cart/cartSlice";
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,6 +16,7 @@ function App() {
 
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
